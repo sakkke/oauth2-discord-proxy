62b6b62df0ecec8ad95d059ecf0ae47642249f0f
sakkke <84666033+sakkke@users.noreply.github.com>
2024-03-21 13:42:50 +0900

    chore: switch to src layout

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import { serve } from 'bun'
 import { Hono } from 'hono'
 import { createBot, createProxy } from './index'
-import { client_id, client_secret, discord_guild_id, discord_token, oauth2_callback, oauth2_endpoint, port } from './config.json'
+import { client_id, client_secret, discord_guild_id, discord_token, oauth2_callback, oauth2_endpoint, port } from '../config.json'
 
 const app = new Hono()
 
